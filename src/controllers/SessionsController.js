82954f84f3bf12026ed0247322c55f85b6e61e89
frederico <fredericonakajimanakajima@gmail.com>
2025-01-05 05:31:55 -0300

    correção rota do signout

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -23,6 +23,7 @@ class SessionsController{
 
 
         const { secret, expiresIn } = authConfig.jwt;
+        
         const token = sign({}, secret, {
             subject: String(user.id),
             expiresIn
